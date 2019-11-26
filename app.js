af2405366b4d5af5df2d1f82afb91e2fdad06835
ProjectX2019 <43288612+ProjectX2019@users.noreply.github.com>
2019-11-26 11:47:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser    = require('body-parser');
 const cors          = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost");
